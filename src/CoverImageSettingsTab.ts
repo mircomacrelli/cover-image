9c3db7fcd5f915cd4c1f7831b343c4bdd8db685a
Mirco Macrelli <mail@mircomacrelli.net>
2026-06-27 05:35:43 +0200

    debounce update

--- src/CoverImageSettingsTab.ts
+++ src/CoverImageSettingsTab.ts
@@ -1,14 +1,21 @@
-import {App, PluginSettingTab, SettingDefinitionItem} from 'obsidian';
+import {App, debounce, Debouncer, PluginSettingTab, Setting, SettingDefinitionItem, TextComponent} from 'obsidian';
 import CoverImage from './CoverImage';
 
 
 export class CoverImageSettingsTab extends PluginSettingTab {
-    plugin: CoverImage;
+    private plugin: CoverImage;
+    private readonly updatePropertyName: Debouncer<[value: string], Promise<void>>;
 
     constructor(app: App, plugin: CoverImage) {
         super(app, plugin);
         this.plugin = plugin;
         this.icon = 'image';
+
+        this.updatePropertyName = debounce(async (value: string): Promise<void> => {
+            this.plugin.settings.propertyName = value;
+            await this.plugin.saveSettings();
+            this.plugin.updateLeaves();
+        }, 500);
     }
 
     getSettingDefinitions(): SettingDefinitionItem[] {
@@ -16,15 +23,13 @@ export class CoverImageSettingsTab extends PluginSettingTab {
             {
                 name: 'Property name',
                 desc: 'Change the property name from which the cover is read',
-                render: (setting) => {
-                    setting.addText((text) =>
+                render: (setting: Setting): void => {
+                    setting.addText((text: TextComponent): TextComponent =>
                         text.setValue(this.plugin.settings.propertyName)
-                            .onChange(async (value) => {
-                                this.plugin.settings.propertyName = value;
-                                this.plugin.updateLeaves();
-                                await this.plugin.saveSettings();
+                            .onChange(async (value: string): Promise<void> => {
+                                this.updatePropertyName(value);
                             })
-                    )
+                    );
                 }
             }
         ];
